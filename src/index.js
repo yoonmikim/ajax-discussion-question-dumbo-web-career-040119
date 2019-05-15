fd7c0fa96ac62870a91f7d7b70e152122cb323da
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-15 15:16:07 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+function fetchData(){
+        var data = fetch('https://randomuser.me/api/')
+          .then( res => res.json() )
+          .then( console.log )
+}
+ 
+fetchData()
